a10e42994a46f40e2bee5620edbe78b0c96db7aa
Wolfy-J <wolfy.jd@gmail.com>
2018-11-07 13:04:15 +0300

    init

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Spiral Framework, SpiralScout LLC.
+ *
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
+
+//Composer
+require dirname(__DIR__) . '/vendor/autoload.php';

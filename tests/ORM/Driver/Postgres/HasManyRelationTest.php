480d3bd5618d566eb201760096b9d47ae4bc271f
Wolfy-J <wolfy.jd@gmail.com>
2018-11-14 14:08:01 +0300

    has many relation

---
+++ tests/ORM/Driver/Postgres/HasManyRelationTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+ 
+namespace Spiral\ORM\Tests\Driver\Postgres; 
+
+class HasManyRelationTest extends \Spiral\ORM\Tests\HasManyRelationTest 
+{ 
+    const DRIVER = "postgres"; 
+}

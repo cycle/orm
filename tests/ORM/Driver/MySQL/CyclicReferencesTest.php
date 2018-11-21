3d1100e107b5cb74cf305db2dbee3e821d9f69f7
Wolfy-J <wolfy.jd@gmail.com>
2018-11-21 10:50:56 +0300

    broken: removed leading relation method

---
+++ tests/ORM/Driver/MySQL/CyclicReferencesTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+ 
+namespace Spiral\ORM\Tests\Driver\MySQL; 
+
+class CyclicReferencesTest extends \Spiral\ORM\Tests\CyclicReferencesTest 
+{ 
+    const DRIVER = "mysql"; 
+}
---
+++ tests/ORM/Driver/Postgres/CyclicReferencesTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+ 
+namespace Spiral\ORM\Tests\Driver\Postgres; 
+
+class CyclicReferencesTest extends \Spiral\ORM\Tests\CyclicReferencesTest 
+{ 
+    const DRIVER = "postgres"; 
+}

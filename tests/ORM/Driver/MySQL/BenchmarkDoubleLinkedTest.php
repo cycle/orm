c0d04b3aa84eda4cf098153a9e717d02058d2dfb
Wolfy-J <wolfy.jd@gmail.com>
2018-12-08 16:22:32 +0300

    points and states

---
+++ tests/ORM/Driver/MySQL/BenchmarkDoubleLinkedTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+ 
+namespace Spiral\ORM\Tests\Driver\MySQL; 
+
+class BenchmarkDoubleLinkedTest extends \Spiral\ORM\Tests\Benchmark\BenchmarkDoubleLinkedTest
+{ 
+    const DRIVER = "mysql"; 
+}
---
+++ tests/ORM/Driver/Postgres/BenchmarkDoubleLinkedTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+ 
+namespace Spiral\ORM\Tests\Driver\Postgres; 
+
+class BenchmarkDoubleLinkedTest extends \Spiral\ORM\Tests\Benchmark\BenchmarkDoubleLinkedTest
+{ 
+    const DRIVER = "postgres"; 
+}

29e7b81d1ee744783a598c0a83c2cf2030ab2e76
Wolfy-J <wolfy.jd@gmail.com>
2018-12-09 19:35:04 +0300

    cs

--- src/DependencyInterface.php
+++ src/DependencyInterface.php
@@ -8,6 +8,9 @@
 
 namespace Spiral\ORM;
 
+/**
+ * Identical to RelationInterface but defines "left" side of the graph (relation to parent objects).
+ */
 interface DependencyInterface extends RelationInterface
 {
 

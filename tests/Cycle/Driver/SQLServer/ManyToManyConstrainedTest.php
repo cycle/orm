acd55035c654d66e180ef8efae66157d8f5c4d2c
Wolfy-J <wolfy.jd@gmail.com>
2019-02-07 17:58:21 +0300

    where conditions and tests for relations

---
+++ tests/Cycle/Driver/SQLServer/ManyToManyConstrainedTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+ 
+namespace Spiral\Cycle\Tests\Driver\SQLServer; 
+
+class ManyToManyConstrainedTest extends \Spiral\Cycle\Tests\ManyToManyConstrainedTest 
+{ 
+    const DRIVER = "sqlserver"; 
+}

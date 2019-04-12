d78ebae888b45a7608dc99f4ca90da5c27abf722
Wolfy-J <wolfy.jd@gmail.com>
2019-04-12 16:12:41 +0300

    updated SQLServer driver version to 5.6.0 to enable full suite test on PHP7.3

--- tests/install-sqlsrv.sh
+++ tests/install-sqlsrv.sh
@@ -4,4 +4,5 @@ pecl install pdo_sqlsrv-5.6.0
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql.list
 sudo apt-get update
+
 ACCEPT_EULA=Y sudo apt-get install -qy msodbcsql17 mssql-tools unixodbc libssl1.0.0

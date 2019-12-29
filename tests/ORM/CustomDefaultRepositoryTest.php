ea1c13bff3dd1d1a3e2368e8c5a423d79a3a3f28
mrakolice <postevoy.svyatoslav@gmail.com>
2019-12-29 17:43:33 +1000

    move orm initialization to setUp

--- tests/ORM/CustomDefaultRepositoryTest.php
+++ tests/ORM/CustomDefaultRepositoryTest.php
@@ -18,29 +18,36 @@ use Spiral\Database\DatabaseManager;
 
 class CustomDefaultRepositoryTest extends TestCase
 {
+    private $orm;
+
+    protected function setUp()
+    {
+        $this->orm = (new ORM(new Factory(new DatabaseManager(new DatabaseConfig()))));
+    }
+
     public function testShouldThrowExceptionIfRepositoryClassNotImplementedRepositoryInterface()
     {
         $this->expectException(RepositoryException::class);
 
-        (new ORM(new Factory(new DatabaseManager(new DatabaseConfig()))))->withDefaultRepository(IncorrectRepository::class);
+        $this->orm->withDefaultRepository(IncorrectRepository::class);
     }
 
     public function testShouldNotThrowExceptionIfRepositoryClassImplementedRepositoryInterface()
     {
-        (new ORM(new Factory(new DatabaseManager(new DatabaseConfig()))))->withDefaultRepository(CorrectRepository::class);
+        $this->orm->withDefaultRepository(CorrectRepository::class);
 
         $this->assertNull($this->getExpectedException());
     }
 
     public function testShouldReturnCorrectDefaultRepository()
     {
-        $orm = (new ORM(new Factory(new DatabaseManager(new DatabaseConfig()))))->withSchema(new Schema([
+        $this->orm = $this->orm->withSchema(new Schema([
             User::class => [
                 1 => 'value',
                 Schema::ROLE => 'user'
             ]
         ]))->withDefaultRepository(CorrectRepository::class);
 
-        $this->assertEquals(CorrectRepository::class, get_class($orm->getRepository('user')));
+        $this->assertEquals(CorrectRepository::class, get_class($this->orm->getRepository('user')));
     }
 }

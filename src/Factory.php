1ddf6b4d586e4ee895b3cc116ab69707a9d22be6
mrakolice <postevoy.svyatoslav@gmail.com>
2020-01-20 15:08:03 +1000

    add possibility to change default Repository, Mapper, Constrain and Source classes in Factory

--- src/Factory.php
+++ src/Factory.php
@@ -12,9 +12,14 @@ declare(strict_types=1);
 namespace Cycle\ORM;
 
 use Cycle\ORM\Config\RelationConfig;
+use Cycle\ORM\Exception\TypecastException;
 use Cycle\ORM\Mapper\Mapper;
 use Cycle\ORM\Relation\RelationInterface;
+use Cycle\ORM\Select\ConstrainInterface;
 use Cycle\ORM\Select\LoaderInterface;
+use Cycle\ORM\Select\Repository;
+use Cycle\ORM\Select\Source;
+use Cycle\ORM\Select\SourceInterface;
 use Psr\Container\ContainerInterface;
 use Spiral\Core\Container;
 use Spiral\Core\FactoryInterface as CoreFactory;
@@ -35,22 +40,32 @@ final class Factory implements FactoryInterface
     /** @var DatabaseProviderInterface */
     private $dbal;
 
+    /** @var array<string, string> */
+    private $defaults = [];
+
     /**
      * @param DatabaseProviderInterface $dbal
-     * @param RelationConfig            $config
-     * @param CoreFactory|null          $factory
-     * @param ContainerInterface|null   $container
+     * @param RelationConfig $config
+     * @param CoreFactory|null $factory
+     * @param ContainerInterface|null $container
      */
     public function __construct(
         DatabaseProviderInterface $dbal,
         RelationConfig $config = null,
         CoreFactory $factory = null,
         ContainerInterface $container = null
-    ) {
+    )
+    {
         $this->dbal = $dbal;
         $this->config = $config ?? RelationConfig::getDefault();
         $this->factory = $factory ?? new Container();
         $this->container = $container ?? new Container();
+        $this->defaults = [
+            Schema::REPOSITORY => Repository::class,
+            Schema::SOURCE => Source::class,
+            Schema::MAPPER => Mapper::class,
+            Schema::CONSTRAIN => null,
+        ];
     }
 
     /**
@@ -68,12 +83,17 @@ final class Factory implements FactoryInterface
         ORMInterface $orm,
         SchemaInterface $schema,
         string $role
-    ): MapperInterface {
-        $class = $schema->define($role, Schema::MAPPER) ?? Mapper::class;
+    ): MapperInterface
+    {
+        $class = $schema->define($role, Schema::MAPPER) ?? $this->defaults[Schema::MAPPER];
+
+        if (!is_subclass_of($class, MapperInterface::class)) {
+            throw new TypecastException($class . ' not implement MapperInterface');
+        }
 
         return $this->factory->make($class, [
-            'orm'    => $orm,
-            'role'   => $role,
+            'orm' => $orm,
+            'role' => $role,
             'schema' => $schema->define($role, Schema::SCHEMA)
         ]);
     }
@@ -86,12 +106,13 @@ final class Factory implements FactoryInterface
         SchemaInterface $schema,
         string $role,
         string $relation
-    ): LoaderInterface {
+    ): LoaderInterface
+    {
         $schema = $schema->defineRelation($role, $relation);
 
         return $this->config->getLoader($schema[Relation::TYPE])->resolve($this->factory, [
-            'orm'    => $orm,
-            'name'   => $relation,
+            'orm' => $orm,
+            'name' => $relation,
             'target' => $schema[Relation::TARGET],
             'schema' => $schema[Relation::SCHEMA]
         ]);
@@ -105,13 +126,14 @@ final class Factory implements FactoryInterface
         SchemaInterface $schema,
         string $role,
         string $relation
-    ): RelationInterface {
+    ): RelationInterface
+    {
         $relSchema = $schema->defineRelation($role, $relation);
         $type = $relSchema[Relation::TYPE];
 
         return $this->config->getRelation($type)->resolve($this->factory, [
-            'orm'    => $orm,
-            'name'   => $relation,
+            'orm' => $orm,
+            'name' => $relation,
             'target' => $relSchema[Relation::TARGET],
             'schema' => $relSchema[Relation::SCHEMA] + [Relation::LOAD => $relSchema[Relation::LOAD] ?? null],
         ]);
@@ -123,5 +145,81 @@ final class Factory implements FactoryInterface
     public function database(string $database = null): DatabaseInterface
     {
         return $this->dbal->database($database);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function repository(
+        ORMInterface $orm,
+        SchemaInterface $schema,
+        string $role,
+        ?Select $select
+    ): RepositoryInterface
+    {
+        $class = $schema->define($role, Schema::REPOSITORY) ?? $this->defaults[Schema::REPOSITORY];
+
+        if (!is_subclass_of($class, RepositoryInterface::class)) {
+            throw new TypecastException($class . ' not implement RepositoryInterface');
+        }
+
+        return $this->factory->make($class, [
+            'orm' => $orm,
+            'role' => $role,
+            'select' => $select
+        ]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function source(
+        ORMInterface $orm,
+        SchemaInterface $schema,
+        string $role
+    ): SourceInterface
+    {
+        $source = $schema->define($role, Schema::SOURCE) ?? $this->defaults[Schema::SOURCE];
+
+        if (!is_subclass_of($source, SourceInterface::class)) {
+            throw new TypecastException($source . ' not implement SourceInterface');
+        }
+
+        if ($source !== Source::class) {
+            return $this->factory->make($source, ['orm' => $orm, 'role' => $role]);
+        }
+
+        $source = new Source(
+            $this->factory->database($schema->define($role, Schema::DATABASE)),
+            $schema->define($role, Schema::TABLE)
+        );
+
+        $constrain = $schema->define($role, Schema::CONSTRAIN) ?? $this->defaults[Schema::CONSTRAIN];
+        if ($constrain !== null) {
+
+            if (!is_subclass_of($constrain, ConstrainInterface::class)) {
+                throw new TypecastException($constrain . ' not implement ConstrainInterface');
+            }
+
+            $source = $source->withConstrain(
+                is_object($constrain) ? $constrain : $this->factory->make($constrain)
+            );
+        }
+
+        return $source;
+    }
+
+    /**
+     * Add default classes for resolve
+     * @param array $defaults
+     * @return FactoryInterface
+     */
+    public function withDefaultSchemaClasses(array $defaults): FactoryInterface
+    {
+        $clone = clone $this;
+
+        $clone->defaults = array_merge($this->defaults, $defaults);
+
+        return $clone;
     }
 }

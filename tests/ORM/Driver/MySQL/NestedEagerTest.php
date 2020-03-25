bad940519dacfcc41a9ecc8fb45172236fd11290
Wolfy-J <wolfy.jd@gmail.com>
2020-03-25 16:29:04 +0300

    - do not load embedded object when parent not loaded

---
+++ tests/ORM/Driver/MySQL/NestedEagerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Cycle DataMapper ORM
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Tests\Driver\MySQL;
+
+class NestedEagerTest extends \Cycle\ORM\Tests\NestedEagerTest
+{
+    public const DRIVER = 'mysql';
+}

4e4be7fea63a76c52cf4182c3eb40fcec45a1326
thenotsoft <44147615+thenotsoft@users.noreply.github.com>
2020-11-27 11:12:33 +0200

    Fix Illegal offset type in src/Heap/Heap.php #140

--- src/Heap/Heap.php
+++ src/Heap/Heap.php
@@ -74,7 +74,7 @@ final class Heap implements HeapInterface, IteratorAggregate
     {
         if (count($scope) === 1) {
             $key = key($scope);
-            return $this->paths[$role][$key][$scope[$key]] ?? null;
+            return $this->paths[$role][$key][(string)$scope[$key]] ?? null;
         }
 
         $key = $value = '';

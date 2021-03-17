854b106e9d4790141add5c197d599671276637dc
roxblnfk <roxblnfk@ya.ru>
2021-03-17 17:11:27 +0300

    Fix test case for mssql

--- tests/ORM/Driver/SQLServer/RenamedPKTest.php
+++ tests/ORM/Driver/SQLServer/RenamedPKTest.php
@@ -6,5 +6,12 @@ namespace Cycle\ORM\Tests\Driver\SQLServer;
 
 class RenamedPKTest extends \Cycle\ORM\Tests\RenamedPKTest
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->getDatabase()->query('SET IDENTITY_INSERT simple_entity ON');
+    }
+
     public const DRIVER = 'sqlserver';
 }

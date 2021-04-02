2ebd021b7745c3639aac5f7d8ffa3f5bcd4f136f
roxblnfk <roxblnfk@ya.ru>
2021-04-02 12:36:54 +0300

    Add tests

---
+++ tests/ORM/Driver/SQLServer/RefersToRelationMiniRowsetTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Tests\Driver\SQLServer;
+
+class RefersToRelationMiniRowsetTest extends \Cycle\ORM\Tests\RefersToRelationMiniRowsetTest
+{
+    public const DRIVER = 'sqlserver';
+}

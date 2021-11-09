84cf875be16e9f6b5ec61c1bf4fc6587b4244531
Maxim Smakouz <m.smakouz@gmail.com>
2021-11-09 17:37:01 +0200

    Change database configs from arrays to dto (#237)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Cycle\Database;
+use Cycle\Database\Config;
 
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', '1');
@@ -11,34 +11,40 @@ ini_set('display_errors', '1');
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $drivers = [
-    'sqlite' => [
-        'driver' => Database\Driver\SQLite\SQLiteDriver::class,
-        'conn' => 'sqlite::memory:',
-        'user' => 'sqlite',
-        'pass' => '',
-        'queryCache' => 100,
-    ],
-    'mysql' => [
-        'driver' => Database\Driver\MySQL\MySQLDriver::class,
-        'conn' => 'mysql:host=127.0.0.1:13306;dbname=spiral',
-        'user' => 'root',
-        'pass' => 'root',
-        'queryCache' => 100,
-    ],
-    'postgres' => [
-        'driver' => Database\Driver\Postgres\PostgresDriver::class,
-        'conn' => 'pgsql:host=127.0.0.1;port=15432;dbname=spiral',
-        'user' => 'postgres',
-        'pass' => 'postgres',
-        'queryCache' => 100,
-    ],
-    'sqlserver' => [
-        'driver' => Database\Driver\SQLServer\SQLServerDriver::class,
-        'conn' => 'sqlsrv:Server=127.0.0.1,11433;Database=tempdb',
-        'user' => 'SA',
-        'pass' => 'SSpaSS__1',
-        'queryCache' => 100,
-    ],
+    'sqlite' => new Config\SQLiteDriverConfig(
+        queryCache: true,
+    ),
+    'mysql' => new Config\MySQLDriverConfig(
+        connection: new Config\MySQL\TcpConnectionConfig(
+            database: 'spiral',
+            host: '127.0.0.1',
+            port: 13306,
+            user: 'root',
+            password: 'root',
+        ),
+        queryCache: true
+    ),
+    'postgres' => new Config\PostgresDriverConfig(
+        connection: new Config\Postgres\TcpConnectionConfig(
+            database: 'spiral',
+            host: '127.0.0.1',
+            port: 15432,
+            user: 'postgres',
+            password: 'postgres',
+        ),
+        schema: 'public',
+        queryCache: true,
+    ),
+    'sqlserver' => new Config\SQLServerDriverConfig(
+        connection: new Config\SQLServer\TcpConnectionConfig(
+            database: 'tempdb',
+            host: '127.0.0.1',
+            port: 11433,
+            user: 'SA',
+            password: 'SSpaSS__1'
+        ),
+        queryCache: true
+    ),
 ];
 
 $db = getenv('DB') ?: null;

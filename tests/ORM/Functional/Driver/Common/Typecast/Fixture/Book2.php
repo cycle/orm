be84a420e16119e77ae96bab45281c3753abec58
butschster <butschster@gmail.com>
2021-11-15 22:15:38 +0300

    Adds unit tests

---
+++ tests/ORM/Functional/Driver/Common/Typecast/Fixture/Book2.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Tests\Functional\Driver\Common\Typecast\Fixture;
+
+class Book2
+{
+    public $id = null;
+    public $title = null;
+    public $description = null;
+}

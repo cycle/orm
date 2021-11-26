3259321d268318b7c9a1d380cb3030d8b238282b
butschster <butschster@gmail.com>
2021-11-26 18:18:51 +0300

    Fixes CS

--- tests/ORM/Driver/Postgres/TransactionTest.php
+++ tests/ORM/Driver/Postgres/TransactionTest.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Cycle DataMapper ORM
- *
- * @license   MIT
- * @author    Anton Titov (Wolfy-J)
- */
+
 declare(strict_types=1);
 
 namespace Cycle\ORM\Tests\Driver\Postgres;
--- tests/generate.php
+++ tests/generate.php
@@ -58,12 +58,7 @@ foreach ($classes as $class) {
             $filename,
             sprintf(
                 '<?php
-/**
- * Cycle DataMapper ORM
- *
- * @license   MIT
- * @author    Anton Titov (Wolfy-J)
- */
+
 declare(strict_types=1);
 
 namespace %s;

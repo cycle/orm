f1b1704a00c280131e9364f5ea727e71f8e5c9cd
roxblnfk <roxblnfk@ya.ru>
2021-12-09 13:48:33 +0300

    Reorganize Runner tests

--- tests/ORM/Functional/Driver/Common/Transaction/RunnerTest.php
+++ tests/ORM/Functional/Driver/Common/Transaction/RunnerTest.php
@@ -19,7 +19,9 @@ abstract class RunnerTest extends BaseTest
         parent::setUp();
     }
 
-    public function testRunOpenTransaction(): void
+    // Mode OPEN_TRANSACTION
+
+    public function testOpenTransactionRun(): void
     {
         $this->assertSame(0, $this->getDriver()->getTransactionLevel());
 
@@ -28,34 +30,10 @@ abstract class RunnerTest extends BaseTest
         $this->assertSame(1, $this->getDriver()->getTransactionLevel());
     }
 
-    public function testRunContinueTransaction(): void
-    {
-        $this->getDriver()->beginTransaction();
-
-        $this->assertSame(1, $this->getDriver()->getTransactionLevel());
-
-        Runner::continueTransaction()->run(new TestCommand($this->getDatabase()));
-
-        $this->assertSame(1, $this->getDriver()->getTransactionLevel());
-    }
-
-    public function testRunContinueNotOpenedTransaction(): void
-    {
-        $this->expectException(RunnerException::class);
-
-        Runner::continueTransaction()->run(new TestCommand($this->getDatabase()));
-    }
-
-    public function testRunIgnoreTransaction(): void
-    {
-        $this->assertSame(0, $this->getDriver()->getTransactionLevel());
-
-        (Runner::ignoreTransaction())->run(new TestCommand($this->getDatabase()));
-
-        $this->assertSame(0, $this->getDriver()->getTransactionLevel());
-    }
-
-    public function testCompleteCloseTransaction(): void
+    /**
+     * The opened transaction should be commited
+     */
+    public function testOpenTransactionComplete(): void
     {
         $runner = Runner::openTransaction();
         $runner->run(new TestCommand($this->getDatabase()));
@@ -67,7 +45,42 @@ abstract class RunnerTest extends BaseTest
         $this->assertSame(0, $this->getDriver()->getTransactionLevel());
     }
 
-    public function testCompleteContinueTransaction(): void
+    /**
+     * The opened transaction should be rollbacked
+     */
+    public function testOpenTransactionRollback(): void
+    {
+        $runner = Runner::openTransaction();
+        $runner->run(new TestCommand($this->getDatabase()));
+
+        $this->assertSame(1, $this->getDriver()->getTransactionLevel());
+
+        $runner->rollback();
+
+        $this->assertSame(0, $this->getDriver()->getTransactionLevel());
+    }
+
+    // Mode CONTINUE_TRANSACTION
+
+    public function testContinueTransactionRun(): void
+    {
+        $this->getDriver()->beginTransaction();
+
+        $this->assertSame(1, $this->getDriver()->getTransactionLevel());
+
+        Runner::continueTransaction()->run(new TestCommand($this->getDatabase()));
+
+        $this->assertSame(1, $this->getDriver()->getTransactionLevel());
+    }
+
+    public function testContinueTransactionWithNotOpenedTransaction(): void
+    {
+        $this->expectException(RunnerException::class);
+
+        Runner::continueTransaction()->run(new TestCommand($this->getDatabase()));
+    }
+
+    public function testContinueTransactionComplete(): void
     {
         $this->getDriver()->beginTransaction();
 
@@ -80,7 +93,31 @@ abstract class RunnerTest extends BaseTest
         $this->assertSame(1, $this->getDriver()->getTransactionLevel());
     }
 
-    public function testCompleteIgnoreTransaction(): void
+    public function testContinueTransactionRollback(): void
+    {
+        $this->getDriver()->beginTransaction();
+
+        $this->assertSame(1, $this->getDriver()->getTransactionLevel());
+
+        $runner = Runner::continueTransaction();
+        $runner->run(new TestCommand($this->getDatabase()));
+        $runner->rollback();
+
+        $this->assertSame(1, $this->getDriver()->getTransactionLevel());
+    }
+
+    // Mode IGNORE_TRANSACTION
+
+    public function testIgnoreTransactionRun(): void
+    {
+        $this->assertSame(0, $this->getDriver()->getTransactionLevel());
+
+        (Runner::ignoreTransaction())->run(new TestCommand($this->getDatabase()));
+
+        $this->assertSame(0, $this->getDriver()->getTransactionLevel());
+    }
+
+    public function testIgnoreTransactionComplete(): void
     {
         $this->getDriver()->beginTransaction();
         $this->assertSame(1, $this->getDriver()->getTransactionLevel());
@@ -92,32 +129,7 @@ abstract class RunnerTest extends BaseTest
         $this->assertSame(1, $this->getDriver()->getTransactionLevel());
     }
 
-    public function testRollbackCloseTransaction(): void
-    {
-        $runner = Runner::openTransaction();
-        $runner->run(new TestCommand($this->getDatabase()));
-
-        $this->assertSame(1, $this->getDriver()->getTransactionLevel());
-
-        $runner->rollback();
-
-        $this->assertSame(0, $this->getDriver()->getTransactionLevel());
-    }
-
-    public function testRollbackContinueTransaction(): void
-    {
-        $this->getDriver()->beginTransaction();
-
-        $this->assertSame(1, $this->getDriver()->getTransactionLevel());
-
-        $runner = Runner::continueTransaction();
-        $runner->run(new TestCommand($this->getDatabase()));
-        $runner->rollback();
-
-        $this->assertSame(1, $this->getDriver()->getTransactionLevel());
-    }
-
-    public function testRollbackIgnoreTransaction(): void
+    public function testIgnoreTransactionRollback(): void
     {
         $this->getDriver()->beginTransaction();
         $this->assertSame(1, $this->getDriver()->getTransactionLevel());

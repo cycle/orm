776852c0b5ddb9d1d695320818ad7f6f7d9059f1
butschster <butschster@gmail.com>
2021-12-28 15:02:12 +0300

    Adds unit tests for typecasts

--- tests/ORM/Unit/Parser/CompositeTypecastTest.php
+++ tests/ORM/Unit/Parser/CompositeTypecastTest.php
@@ -4,8 +4,98 @@ declare(strict_types=1);
 
 namespace Cycle\ORM\Tests\Unit\Parser;
 
+use Cycle\ORM\Parser\CastableInterface;
+use Cycle\ORM\Parser\CompositeTypecast;
+use Cycle\ORM\Parser\TypecastInterface;
+use Cycle\ORM\Parser\UncastableInterface;
+use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
 class CompositeTypecastTest extends TestCase
 {
+    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+    public function testSetRules()
+    {
+        $rules = $initialRules = [
+            'foo' => 'bar',
+            'baz' => 'bar',
+            'bar' => 'bar',
+        ];
+
+        $casters = [
+            [
+                'typecast' => m::mock(TypecastInterface::class),
+                'expects' => $rules,
+                'returns' => [
+                    'foo1' => 'bar',
+                    'baz' => 'bar',
+                    'bar' => 'bar',
+                ],
+            ],
+            [
+                'typecast' => m::mock(CastableInterface::class),
+                'returns' => [
+                    'foo1' => 'bar',
+                    'baz' => 'bar',
+                    'bar' => 'bar',
+                ],
+            ],
+            [
+                'typecast' => m::mock(UncastableInterface::class),
+                'returns' => [
+                    'foo' => 'bar',
+                    'bar' => 'bar',
+                ],
+            ],
+            [
+                'typecast' => m::mock(CastableInterface::class, UncastableInterface::class),
+                'returns' => [
+                    'baz' => 'bar',
+                ],
+            ],
+        ];
+
+        $castData = [
+            'foo' => 'bar',
+        ];
+
+        $uncastData = [
+            'baz' => 'bar',
+        ];
+
+        foreach ($casters as $data) {
+            $typecast = $data['typecast'];
+
+            $typecast->shouldReceive('setRules')->once()
+                ->with($rules)
+                ->andReturn($data['returns']);
+
+            if ($typecast instanceof CastableInterface) {
+                $typecast->shouldReceive('cast')->once()->with($castData)->andReturn($castData);
+            }
+
+            if ($typecast instanceof UncastableInterface) {
+                $typecast->shouldReceive('uncast')->once()->with($uncastData)->andReturn($uncastData);
+            }
+
+            $rules = $data['returns'];
+        }
+
+        $typecast = new CompositeTypecast(
+            ... array_map(static fn(array $data) => $data['typecast'], $casters)
+        );
+
+        $this->assertSame([
+            'baz' => 'bar',
+        ], $typecast->setRules($initialRules));
+
+        $this->assertSame($castData, $typecast->cast($castData));
+        $this->assertSame($uncastData, $typecast->uncast($uncastData));
+    }
+
+    public function tearDown(): void
+    {
+        m::close();
+    }
 }
--- tests/ORM/Unit/Parser/TypecastTest.php
+++ tests/ORM/Unit/Parser/TypecastTest.php
@@ -5,36 +5,102 @@ declare(strict_types=1);
 namespace Cycle\ORM\Tests\Unit\Parser;
 
 use Cycle\Database\DatabaseInterface;
+use Cycle\Database\Driver\DriverInterface;
 use Cycle\ORM\Parser\Typecast;
 use Cycle\ORM\Tests\Fixtures\Uuid;
+use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
 class TypecastTest extends TestCase
 {
     private Typecast $typecast;
+    private \Mockery\LegacyMockInterface|\Mockery\MockInterface|DatabaseInterface $db;
 
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->typecast = new Typecast(
-            $this->createMock(DatabaseInterface::class)
+            $this->db = m::mock(DatabaseInterface::class)
         );
     }
 
-    public function testApplyRules()
+    public function testSetRules()
     {
         $rules = [
             'id' => 'int',
             'guest' => 'bool',
             'bonus' => 'float',
             'date' => 'datetime',
-            'slug' => fn (string $value) => strtolower($value),
+            'slug' => fn(string $value) => strtolower($value),
             'title' => 'strtoupper',
             'test' => [Uuid::class, 'create'],
             'uuid' => 'uuid',
         ];
 
         $this->assertSame(['uuid' => 'uuid'], $this->typecast->setRules($rules));
+    }
+
+    public function testCastIntValue()
+    {
+        $this->typecast->setRules(['id' => 'int']);
+
+        $this->assertSame([
+            'id' => 10,
+            'foo' => '5'
+        ], $this->typecast->cast(['id' => '10', 'foo' => '5']));
+    }
+
+    public function testCastBoolValue()
+    {
+        $this->typecast->setRules(['is_admin' => 'bool']);
+
+        $this->assertSame([
+            'is_admin' => false,
+            'foo' => 1
+        ], $this->typecast->cast(['is_admin' => 0, 'foo' => 1]));
+    }
+
+    public function testCastFloatValue()
+    {
+        $this->typecast->setRules(['price' => 'float']);
+
+        $this->assertSame([
+            'price' => 100.0,
+            'foo' => '5'
+        ], $this->typecast->cast(['price' => 100, 'foo' => '5']));
+    }
+
+    public function testCasDateTimeValue()
+    {
+        $this->db->shouldReceive('getDriver')->once()->andReturn($driver = m::mock(DriverInterface::class));
+        $driver->shouldReceive('getTimezone')->once()->andReturn(new \DateTimeZone('Europe/Berlin'));
+
+        $this->typecast->setRules(['date' => 'datetime']);
+
+        $data = $this->typecast->cast(['date' => '2010-05-10 12:04:10', 'foo' => '2010-05-10']);
+
+        $this->assertSame('2010-05-10T12:04:10+02:00', $data['date']->format(\DateTimeInterface::ATOM));
+        $this->assertSame('2010-05-10', $data['foo']);
+    }
+
+    public function testCastValueWithNonExistType()
+    {
+        $this->typecast->setRules(['foo' => 'bar']);
+
+        $this->assertSame([
+            'price' => '10000',
+        ], $this->typecast->cast(['price' => '10000']));
+    }
+
+    public function testCastCallableValue()
+    {
+        $this->typecast->setRules(['uuid' => [Uuid::class, 'parse']]);
+
+        $uuid = \Ramsey\Uuid\Uuid::fromString('71ceb213-ec3d-4ae5-911b-ba042abfb204');
+        $result = $this->typecast->cast(['uuid' => $uuid->getBytes()]);
+
+        $this->assertInstanceOf(Uuid::class, $result['uuid']);
+        $this->assertSame('71ceb213-ec3d-4ae5-911b-ba042abfb204', $result['uuid']->toString());
     }
 }

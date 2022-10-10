2446dfb6006d45708fb915b830dffcd2313fbc4d
Aleksei Gagarin <roxblnfk@ya.ru>
2022-10-10 11:00:55 +0300

    Mark `Repository::$select` readonly (#369)

--- src/Transaction/StateInterface.php
+++ src/Transaction/StateInterface.php
@@ -5,20 +5,24 @@ declare(strict_types=1);
 namespace Cycle\ORM\Transaction;
 
 use Cycle\ORM\Exception\SuccessTransactionRetryException;
+use Throwable;
 
 interface StateInterface
 {
     /**
      * Check if transaction has been run successful.
+     *
+     * @psalm-assert-if-true null $this->getLastError()
+     * @psalm-assert-if-false Throwable $this->getLastError()
      */
     public function isSuccess(): bool;
 
     /**
      * The reason of failed transaction.
      *
-     * @return \Throwable|null
+     * @return Throwable|null
      */
-    public function getLastError(): ?\Throwable;
+    public function getLastError(): ?Throwable;
 
     /**
      * Try to rerun transaction if previous run has been failed.

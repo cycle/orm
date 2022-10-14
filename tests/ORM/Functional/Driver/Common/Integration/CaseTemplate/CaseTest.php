34defca3bd67e77a70778b7396a066b3b6124003
roxblnfk <roxblnfk@ya.ru>
2022-10-14 18:11:47 +0300

    Add CaseTemplate

---
+++ tests/ORM/Functional/Driver/Common/Integration/CaseTemplate/CaseTest.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Tests\Functional\Driver\Common\Integration\CaseTemplate;
+
+use Cycle\ORM\Schema;
+use Cycle\ORM\Select;
+use Cycle\ORM\Tests\Functional\Driver\Common\BaseTest;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\CaseTemplate\Entity;
+use Cycle\ORM\Tests\Traits\TableTrait;
+
+abstract class CaseTest extends BaseTest
+{
+    use TableTrait;
+
+    public function setUp(): void
+    {
+        // Init DB
+        parent::setUp();
+        $this->makeTables();
+        $this->fillData();
+
+        $schema = include __DIR__ . '/schema.php';
+        assert(\is_array($schema));
+        $this->orm = $this->orm->withSchema(new Schema($schema));
+    }
+
+    public function testRun(): void
+    {
+        $users = (new Select($this->orm, Entity\User::class))
+            ->load('posts')
+            ->fetchAll();
+
+        $this->assertCount(1, $users);
+        $this->assertCount(2, $users[1]);
+    }
+
+    private function makeTables(): void
+    {
+        // Make tables
+        $this->makeTable(Entity\User::ROLE, [
+            'id' => 'primary', // autoincrement
+            'login' => 'string',
+            'password_hash' => 'string',
+        ]);
+
+        $this->makeTable('post', [
+            'id' => 'primary',
+            'user_id' => 'int',
+            'slug' => 'string',
+            'title' => 'string',
+            'public' => 'bool',
+            'content' => 'string',
+            'published_at' => 'datetime,nullable',
+        ]);
+        $this->makeFK('post', 'user_id', 'user', 'id', 'NO ACTION', 'NO ACTION');
+
+        $this->makeTable('comment', [
+            'id' => 'primary',
+            'public' => 'bool',
+            'content' => 'string',
+            'user_id' => 'int',
+            'post_id' => 'int',
+        ]);
+        $this->makeFK('comment', 'user_id', 'user', 'id', 'NO ACTION', 'NO ACTION');
+        $this->makeFK('comment', 'post_id', 'post', 'id', 'NO ACTION', 'NO ACTION');
+
+        $this->makeTable('tag', [
+            'id' => 'primary',
+            'label' => 'string',
+        ]);
+
+        $this->makeTable('post_tag', [
+            'post_id' => 'int',
+            'tag_id' => 'int',
+        ], pk: ['post_id', 'tag_id']);
+        $this->makeFK('post_tag', 'post_id', 'post', 'id', 'NO ACTION', 'CASCADE');
+        $this->makeFK('post_tag', 'tag_id', 'tag', 'id', 'NO ACTION', 'CASCADE');
+    }
+
+    private function fillData(): void
+    {
+        $this->getDatabase()->table('user')->insertMultiple(
+            ['login', 'password_hash'],
+            [
+                ['user-1', '123456789'],
+                ['user-2', '852741963'],
+                ['user-3', '321654987'],
+                ['user-4', '321456987'],
+            ],
+        );
+        $this->getDatabase()->table('post')->insertMultiple(
+            ['user_id', 'slug', 'title', 'public', 'content'],
+            [
+                [1, 'slug-string-1', 'Title 1', true, 'Foo-bar-baz content 1'],
+                [2, 'slug-string-2', 'Title 2', true, 'Foo-bar-baz content 2'],
+                [2, 'slug-string-3', 'Title 3', true, 'Foo-bar-baz content 3'],
+                [3, 'slug-string-4', 'Title 4', true, 'Foo-bar-baz content 4'],
+                [3, 'slug-string-5', 'Title 5', true, 'Foo-bar-baz content 5'],
+                [3, 'slug-string-6', 'Title 6', true, 'Foo-bar-baz content 6'],
+            ],
+        );
+    }
+}
---
+++ tests/ORM/Functional/Driver/Common/Integration/CaseTemplate/Entity/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Tests\Functional\Driver\Common\Integration\CaseTemplate\Entity;
+
+use DateTimeImmutable;
+
+class Post
+{
+    public ?int $id = null;
+    public string $slug;
+    public string $title = '';
+    public bool $public = false;
+    public string $content = '';
+    public DateTimeImmutable $created_at;
+    public DateTimeImmutable $updated_at;
+    public ?DateTimeImmutable $published_at = null;
+    public ?DateTimeImmutable $deleted_at = null;
+    public User $user;
+    public ?int $user_id = null;
+    /** @var Tag[] */
+    public array $tags = [];
+    public ?int $tag_id = null;
+    /** @var Comment[] */
+    public array $comments = [];
+
+    public function __construct(string $title = '', string $content = '')
+    {
+        $this->title = $title;
+        $this->content = $content;
+        $this->created_at = new DateTimeImmutable();
+        $this->updated_at = new DateTimeImmutable();
+        $this->resetSlug();
+    }
+
+    public function resetSlug(): void
+    {
+        $this->slug = \bin2hex(\random_bytes(32));
+    }
+}

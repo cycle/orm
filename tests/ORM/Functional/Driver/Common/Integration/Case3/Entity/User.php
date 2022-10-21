4987962201938eb8beb880f2ec12fb55606846d9
Maxim Smakouz <m.smakouz@gmail.com>
2022-10-21 15:26:12 +0300

    Add TestCase

---
+++ tests/ORM/Functional/Driver/Common/Integration/Case3/Entity/User.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case3\Entity;
+
+class User
+{
+    public int $id;
+    public string $name;
+    public Currency $currency;
+}
---
+++ tests/ORM/Functional/Driver/Common/Integration/Case3/schema.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+use Cycle\ORM\Mapper\Mapper;
+use Cycle\ORM\Relation;
+use Cycle\ORM\SchemaInterface as Schema;
+use Cycle\ORM\Select\Source;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case3\Entity\Currency;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case3\Entity\User;
+
+return [
+    'currency' => [
+        Schema::ENTITY => Currency::class,
+        Schema::SOURCE => Source::class,
+        Schema::DATABASE => 'default',
+        Schema::TABLE => 'currencies',
+        Schema::PRIMARY_KEY => ['code'],
+        Schema::FIND_BY_KEYS => ['code'],
+        Schema::COLUMNS => [
+            'code' => 'code',
+            'name' => 'name',
+        ],
+    ],
+    'user' => [
+        Schema::ENTITY => User::class,
+        Schema::SOURCE => Source::class,
+        Schema::DATABASE => 'default',
+        Schema::MAPPER => Mapper::class,
+        Schema::TABLE => 'users',
+        Schema::PRIMARY_KEY => ['id'],
+        Schema::FIND_BY_KEYS => ['id'],
+        Schema::COLUMNS => [
+            'id' => 'id',
+            'name' => 'name',
+            'currency_code' => 'currency_code'
+        ],
+        Schema::RELATIONS => [
+            'currency' => [
+                Relation::TYPE => Relation::BELONGS_TO,
+                Relation::TARGET => 'currency',
+                Relation::LOAD => Relation::LOAD_EAGER,
+                Relation::SCHEMA => [
+                    Relation::CASCADE => false,
+                    Relation::NULLABLE => false,
+                    Relation::INNER_KEY => 'currency_code',
+                    Relation::OUTER_KEY => ['code'],
+                ],
+            ],
+        ],
+        Schema::TYPECAST => [
+            'id' => 'int',
+        ],
+    ],
+];

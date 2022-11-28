7cf91b807835d9d906d2a24892812f5a0bd49ed0
BelaRyc <BelaRyc@gmail.com>
2022-11-28 19:17:59 +0300

    [tests] heap bug

---
+++ tests/ORM/Functional/Driver/Common/Integration/Case5/Entity/PostTag.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case5\Entity;
+
+class PostTag
+{
+    // private ?int $id = null;
+    private ?int $post_id = null;
+    private ?int $tag_id = null;
+}

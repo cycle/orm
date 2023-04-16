aa4b86cf2a64bd0e90a739d98121516b6e6da3c0
BelaRyc <BelaRyc@gmail.com>
2023-04-16 16:40:08 +0300

    tests for #321

---
+++ tests/ORM/Functional/Driver/Common/Integration/Case321/schema.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use Cycle\ORM\Mapper\Mapper;
+use Cycle\ORM\SchemaInterface as Schema;
+use Cycle\ORM\Select\Source;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case321\Entity\User1;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case321\Entity\User2;
+
+return [
+    'user1' => [
+        Schema::ENTITY => User1::class,
+        Schema::MAPPER => Mapper::class,
+        Schema::SOURCE => Source::class,
+        Schema::DATABASE => 'default',
+        Schema::TABLE => 'user1',
+        Schema::PRIMARY_KEY => ['id'],
+        Schema::FIND_BY_KEYS => ['id'],
+        Schema::COLUMNS => [
+            'id' => 'id',
+        ],
+        Schema::RELATIONS => [],
+        Schema::SCOPE => null,
+        Schema::TYPECAST => [
+            'id' => 'int',
+        ],
+        Schema::SCHEMA => [],
+    ],
+    'user2' => [
+        Schema::ENTITY => User2::class,
+        Schema::MAPPER => Mapper::class,
+        Schema::SOURCE => Source::class,
+        Schema::DATABASE => 'default',
+        Schema::TABLE => 'user2',
+        Schema::PRIMARY_KEY => ['id'],
+        Schema::FIND_BY_KEYS => ['id'],
+        Schema::COLUMNS => [
+            'id' => 'id',
+        ],
+        Schema::RELATIONS => [],
+        Schema::SCOPE => null,
+        Schema::TYPECAST => [
+            'id' => 'int',
+        ],
+        Schema::SCHEMA => [],
+    ],
+];

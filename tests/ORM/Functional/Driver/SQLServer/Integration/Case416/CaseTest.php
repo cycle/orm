d957fb5cedaeef3f328e32c374eb8cad0428286a
roxblnfk <roxblnfk@ya.ru>
2023-06-19 21:29:55 +0400

    Add test case with bad BelongsTo hydration

---
+++ tests/ORM/Functional/Driver/SQLServer/Integration/Case416/CaseTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Tests\Functional\Driver\SQLServer\Integration\Case416;
+
+// phpcs:ignore
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case416\CaseTest as CommonClass;
+
+/**
+ * @group driver
+ * @group driver-sqlserver
+ */
+class CaseTest extends CommonClass
+{
+    public const DRIVER = 'sqlserver';
+}

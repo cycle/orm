d741b77b0167020b982d52e55c3f1657e4c5cd03
Maxim Smakouz <m.smakouz@gmail.com>
2023-06-26 20:03:25 +0300

    Add mtm with uuid test case

---
+++ tests/ORM/Functional/Driver/Common/Integration/Case318/CaseTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case318;
+
+use Cycle\ORM\Tests\Functional\Driver\Common\BaseTest;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\IntegrationTestTrait;
+use Cycle\ORM\Tests\Traits\TableTrait;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * @link https://github.com/cycle/orm/issues/318
+ */
+abstract class CaseTest extends BaseTest
+{
+    use IntegrationTestTrait;
+    use TableTrait;
+
+    public function setUp(): void
+    {
+        // Init DB
+        parent::setUp();
+        $this->makeTables();
+
+        $this->loadSchema(__DIR__ . '/schema.php');
+    }
+
+    public function testSelect(): void
+    {
+        $groupUuid = Uuid::uuid4();
+        $userUuid = Uuid::uuid4();
+
+        $this
+            ->getDatabase()
+            ->table('users')
+            ->insertOne(['uuid' => $userUuid->toString(), 'login' => 'user_1']);
+        $this
+            ->getDatabase()
+            ->table('groups')
+            ->insertMultiple(['uuid', 'title'], [[$groupUuid->toString(), 'group_1']]);
+        $this
+            ->getDatabase()
+            ->table('user_groups')
+            ->insertMultiple(['user_uuid', 'group_uuid'], [[$userUuid->toString(), $groupUuid->toString()]]);
+
+        $user = $this->orm->getRepository(Entity\User::class)
+            ->findByPK($userUuid);
+
+        $this->assertEquals($groupUuid, $user->groups[0]->uuid);
+    }
+
+    private function makeTables(): void
+    {
+        // Make tables
+        $this->makeTable('users', [
+            'uuid' => 'string',
+            'login' => 'string',
+        ], pk: ['uuid']);
+
+        $this->makeTable('groups', [
+            'uuid' => 'string',
+            'title' => 'string',
+        ], pk: ['uuid']);
+
+        $this->makeTable('user_groups', [
+            'user_uuid' => 'string',
+            'group_uuid' => 'string',
+        ], pk: ['user_uuid', 'group_uuid']);
+    }
+}
---
+++ tests/ORM/Functional/Driver/Common/Integration/Case318/schema.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+use Cycle\ORM\Relation;
+use Cycle\ORM\SchemaInterface as Schema;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case318\Entity\Group;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case318\Entity\User;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case318\Entity\UserGroup;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case318\Typecast;
+
+return [
+    'user' => [
+        Schema::ENTITY => User::class,
+        Schema::TABLE => 'users',
+        Schema::PRIMARY_KEY => ['uuid'],
+        Schema::COLUMNS => [
+            'uuid' => 'uuid',
+            'login' => 'login',
+        ],
+        Schema::RELATIONS => [
+            'groups' => [
+                Relation::TYPE => Relation::MANY_TO_MANY,
+                Relation::TARGET => Group::class,
+                Relation::SCHEMA => [
+                    Relation::CASCADE => true,
+                    Relation::NULLABLE => false,
+                    Relation::INNER_KEY => 'uuid',
+                    Relation::OUTER_KEY => 'uuid',
+                    Relation::THROUGH_OUTER_KEY => 'group_uuid',
+                    Relation::THROUGH_INNER_KEY => 'user_uuid',
+                    Relation::THROUGH_ENTITY => UserGroup::class,
+                ],
+            ],
+        ],
+        Schema::TYPECAST => [
+            'uuid' => 'uuid',
+        ],
+        Schema::TYPECAST_HANDLER => [
+            Typecast::class
+        ],
+    ],
+    'group' => [
+        Schema::ENTITY => Group::class,
+        Schema::TABLE => 'groups',
+        Schema::PRIMARY_KEY => ['uuid'],
+        Schema::COLUMNS => [
+            'uuid' => 'uuid',
+            'title' => 'title',
+        ],
+        Schema::TYPECAST => [
+            'uuid' => 'uuid',
+        ],
+        Schema::TYPECAST_HANDLER => [
+            Typecast::class
+        ],
+    ],
+    'user_group' => [
+        Schema::ENTITY => UserGroup::class,
+        Schema::DATABASE => 'default',
+        Schema::TABLE => 'user_groups',
+        Schema::PRIMARY_KEY => ['user_uuid', 'group_uuid'],
+        Schema::COLUMNS => [
+            'user_uuid' => 'user_uuid',
+            'group_uuid' => 'group_uuid',
+        ],
+        Schema::TYPECAST => [
+            'user_uuid' => 'uuid',
+            'group_uuid' => 'uuid',
+        ],
+        Schema::TYPECAST_HANDLER => [
+            Typecast::class
+        ],
+    ],
+];

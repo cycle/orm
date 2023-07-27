00b88f0f72bdad0ec9c990e191d70d1e970d2c4e
roxblnfk <roxblnfk@ya.ru>
2023-07-27 20:16:16 +0400

    Fix CS

--- tests/ORM/Functional/Driver/Common/Integration/Case4/CaseTest.php
+++ tests/ORM/Functional/Driver/Common/Integration/Case4/CaseTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Cycle\ORM\Tests\Functional\Driver\Common\Integration\Case4;
 
-use Cycle\ORM\EntityManager;
 use Cycle\ORM\Select;
 use Cycle\ORM\Tests\Functional\Driver\Common\BaseTest;
 use Cycle\ORM\Tests\Functional\Driver\Common\Integration\IntegrationTestTrait;

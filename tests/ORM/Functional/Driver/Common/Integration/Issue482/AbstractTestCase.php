518d84659af99d85988a47af8d0976b712a2a45f
gam6itko <gam6itko@gmail.com>
2024-05-25 18:06:00 +0300

    oops2

--- tests/ORM/Functional/Driver/Common/Integration/Issue482/AbstractTestCase.php
+++ tests/ORM/Functional/Driver/Common/Integration/Issue482/AbstractTestCase.php
@@ -126,12 +126,10 @@ abstract class AbstractTestCase extends BaseTest
         $this->getDatabase()->table('locale')->delete()->run();
 
         $en = 1;
-        $ru = 2;
         $this->getDatabase()->table('locale')->insertMultiple(
             ['code'],
             [
                 ['en'],
-                ['ru'],
             ],
         );
         $this->getDatabase()->table('country')->insertMultiple(
@@ -147,11 +145,8 @@ abstract class AbstractTestCase extends BaseTest
             ['country_id', 'locale_id', 'title'],
             [
                 [1, $en, 'Russia on english'],
-                [1, $ru, 'Россия на русском'],
                 [2, $en, 'America on english'],
-                [2, $ru, 'Америка на русском'],
                 [3, $en, 'China on english'],
-                [3, $ru, 'Китай на русском'],
             ],
         );
     }

97ee1ee35325031a169925bfa65ce87a85c917da
Alexander Strizhak <gam6itko@gmail.com>
2025-03-31 20:10:58 +0300

    Add a test case for issue 511

---
+++ tests/ORM/Functional/Driver/Common/Integration/Issue511/Entity/Passport.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Tests\Functional\Driver\Common\Integration\Issue511\Entity;
+
+class Passport
+{
+    public ?int $id = null;
+
+    public string $number;
+
+    public User $user;
+}

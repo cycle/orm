509246096fa3c655435dfa9a11b3c07337f2e05c
Alexander Strizhak <gam6itko@gmail.com>
2025-04-24 17:08:11 +0300

    issue load eager test

--- tests/ORM/Functional/Driver/Common/Integration/Issue512/Entity/Tag.php
+++ tests/ORM/Functional/Driver/Common/Integration/Issue512/Entity/Tag.php
@@ -8,7 +8,6 @@ class Tag
 {
     public ?int $id = null;
     public string $label;
-    public \DateTimeImmutable $created_at;
 
     /** @var iterable<Post> */
     public iterable $posts = [];
@@ -16,6 +15,5 @@ class Tag
     public function __construct(string $label)
     {
         $this->label = $label;
-        $this->created_at = new \DateTimeImmutable();
     }
 }

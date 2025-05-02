a80ec11944b9f616cb440353d0aac84c0482e69b
roxblnfk <roxblnfk@ya.ru>
2025-05-02 14:38:38 +0400

    Comment tests

--- tests/ORM/Functional/Driver/Common/Integration/Case7/CaseTest.php
+++ tests/ORM/Functional/Driver/Common/Integration/Case7/CaseTest.php
@@ -22,19 +22,46 @@ abstract class CaseTest extends BaseTest
 
     public function testCreatePostEntity(): void
     {
-        // save post
-        $post = new Post('title3', 'content3');
-        (new EntityManager($this->orm))->persist($post)->run();
+        // Create and save post
 
-        // save post tag
-        $pt1 = new PostTag();
+        /**
+         * Wrong way
+         *
+         * @note When we create a new Post entity manually, ORM has no ability to put a promise in relation.
+         *       So, when we call $post->postTags, it will be empty.
+         * @link https://spiral.dev/blog/cycle-orm-hungry-for-relations
+         */
+        // $post = new Post('title3', 'content3');
+        // $this->save($post);
+
+        /**
+         * Right way
+         *
+         * @note We use ORM to create a new Post entity.
+         *       We get an entity-proxy that has a promise in relation under the hood.
+         */
+        $post = $this->orm->make(Post::class, ['title' => 'title3', 'content' => 'content3']);
+
+
+        // Get tag
+        $tag = $this->orm->get(Tag::class, ['id' => 1]);
+        self::assertSame('foo', $tag->label);
+
+        // Save PostTag
+        $pt1 = $this->orm->make(PostTag::class);
         $pt1->post = $post;
-        $pt1->tag = $this->orm->get(Tag::class, ['id' => 1]);
-        (new EntityManager($this->orm))->persist($pt1)->run();
+        $pt1->tag = $tag;
+        $this->save($pt1);
 
         $id = $post->id;
         unset($post);
 
+        /**
+         * @note When we use repository to get a Post entity, we actually get the same cached entity from the heap.
+         *       So, the empty relation won't be overwritten.
+         *
+         *       We can call `$this->orm->getHeap()->clean();` to clear the heap.
+         */
         $post = $this->orm->getRepository(Post::class)->findByPK($id);
         self::assertCount(1, $post->postTags);
     }

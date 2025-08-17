958d49edda2764b8affc4cf7e94a7b5fec511bb6
roxblnfk <roxblnfk@ya.ru>
2025-08-17 16:21:24 +0400

    Rename CaseTest to TestCase for consistency across database driver tests

--- tests/ORM/Functional/Driver/SQLite/Integration/CaseTemplate/TestCase.php
+++ tests/ORM/Functional/Driver/SQLite/Integration/CaseTemplate/TestCase.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace Cycle\ORM\Tests\Functional\Driver\SQLite\Integration\CaseTemplate;
 
 // phpcs:ignore
-use Cycle\ORM\Tests\Functional\Driver\Common\Integration\CaseTemplate\CaseTest as CommonClass;
+use Cycle\ORM\Tests\Functional\Driver\Common\Integration\CaseTemplate\TestCase as CommonClass;
 
 /**
  * @group driver
  * @group driver-sqlite
  */
-class CaseTest extends CommonClass
+class TestCase extends CommonClass
 {
     public const DRIVER = 'sqlite';
 }
